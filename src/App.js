03529922b23030d45dc45346b99e3d98b126406a
RafaelKulka <rafaelkulka9879@gmail.com>
2021-08-27 18:25:36 -0300

    add footer

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    {/*<header className="App-header"> 
+      <header> 
         
-      </header>*/}
+      </header>
 
       <div id="container">
         <p>Asiaa</p>
