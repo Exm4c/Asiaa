f37fb8230c27025155f0814e1d0717360f526d00
Exm4c <yurimaximoto1@gmail.com>
2021-08-30 12:51:20 -0300

    Minor change add menuitems

---
+++ src/components/menuitems.js
@@ -0,0 +1,19 @@
+export const Menuitems = [
+    
+    {
+    product: 'Fones',
+    path: '/fones',
+    className:'dropdown-link'
+   },
+   {
+    product: 'Eletrônicos',
+    path: '/eletronicos',
+    className:'dropdown-link'
+   },
+   {
+    product: 'Celulares',
+    path: '/celulares',
+    className:'dropdown-link'
+   },
+]
+
