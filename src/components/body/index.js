d9cc196fc496bb8836dbe5b01c44e47bb905805f
RafaelKulka <rafaelkulka9879@gmail.com>
2021-08-30 15:51:25 -0300

    ajuste no contudo

--- src/components/body/index.js
+++ src/components/body/index.js
@@ -2,13 +2,40 @@ import React from 'react';
 import './body.css';
 import Products from '../../products';
 import img from '../../icons/test2.png'
-import imgd from '../../icons/test3.png'
 
 function Body (){
     return(
 
         <div className="body">
-            <img src={img}></img>
+           <div clasName="body-container">
+            <div className="body-row">
+                {/*Podem ser quantos produtos quiser, responsivo*/}
+                <Products></Products>
+                <Products></Products>
+                <Products></Products>
+                <Products></Products>
+                <Products></Products>
+                {/*Produtos*/}
+                {/*Produtos*/}
+                {/*Produtos*/}
+                
+            </div>
+            
+            <div className="body-row">
+                <Products></Products>
+                <Products></Products>
+                <Products></Products>
+                <Products></Products>
+                {/*Produtos*/}
+                {/*Produtos*/}
+                {/*Produtos*/}
+                {/*Produtos*/}
+                
+            </div>
+          </div>
+
+            
+
         </div>               
 
     );
