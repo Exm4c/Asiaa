f36dcc6d78c5ac3ed7a62c16306b248bd77fc2c1
RafaelKulka <rafaelkulka9879@gmail.com>
2021-08-30 14:08:30 -0300

    adicionando o conteudo

--- src/components/body/index.js
+++ src/components/body/index.js
@@ -1,39 +1,15 @@
 import React from 'react';
 import './body.css';
 import Products from '../../products';
+import img from '../../icons/test2.png'
+import imgd from '../../icons/test3.png'
 
 function Body (){
     return(
-        
+
         <div className="body">
-            <div clasName="body-container">
-            <div className="body-row">
-                {/*Podem ser quantos produtos quiser, responsivo*/}
-                <Products></Products>
-                <Products></Products>
-                <Products></Products>
-                <Products></Products>
-                <Products></Products>
-                {/*Produtos*/}
-                {/*Produtos*/}
-                {/*Produtos*/}
-                
-            </div>
-            
-            <div className="body-row">
-                <Products></Products>
-                <Products></Products>
-                <Products></Products>
-                <Products></Products>
-                {/*Produtos*/}
-                {/*Produtos*/}
-                {/*Produtos*/}
-                {/*Produtos*/}
-                
-            </div>
-          </div>
-        </div>       
-        
+            <img src={img}></img>
+        </div>               
 
     );
 }
